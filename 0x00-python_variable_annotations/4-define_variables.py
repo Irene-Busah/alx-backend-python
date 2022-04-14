2cfd82517c304e1743dad03c3b12579a15eb8e6e
Irene-Busah <i.busah@alustudent.com>
2022-04-15 00:47:33 +0200

    defining variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""The function implement the concept of declaring variables"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotation: bool = True
+school: str = "Holberton"
