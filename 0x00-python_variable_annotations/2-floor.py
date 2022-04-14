987c72e0e1730f7f3c08c5d1032bcb024d7c00b0
Irene-Busah <i.busah@alustudent.com>
2022-04-15 00:22:25 +0200

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""The function takes a float number as parameter"""
+import math
+
+
+def floor(n: float) -> int:
+    """accepts a floating number as parameter"""
+    return math.floor(n)
