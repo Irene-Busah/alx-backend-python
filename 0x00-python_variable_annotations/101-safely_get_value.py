21f26298f59e4cdd251588f2504ce30844b88e31
Irene-Busah <i.busah@alustudent.com>
2022-04-15 14:27:08 +0200

    safely get value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,7 +2,7 @@
 """module element length"""
 
 
-def safely_get_value(dct, key, default = None):
+def safely_get_value(dct, key, default=None):
     """safely get value"""
     if key in dct:
         return dct[key]
